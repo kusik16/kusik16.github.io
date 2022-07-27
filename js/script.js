472a0d650893933857027c7d704814fb9dd90e85
kusik16 <slobodyanuck@gmail.com>
2022-07-27 17:29:56 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector(".hamburger"),
+    menu = document.querySelector(".menu"),
+    closeElem = document.querySelector(".menu__close");
+    overlay = document.querySelector(".menu__overlay");
+    links = document.querySelectorAll(".menu__link");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.add("active");
+});
+
+closeElem.addEventListener("click", () => {
+    menu.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+    menu.classList.remove("active");
+});
+
+const counters = document.querySelectorAll(".skills__ratings-counter"),
+    lines = document.querySelectorAll(".skills__ratings-line span");
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+links.forEach((item) => {
+    item.addEventListener("click", () => {
+        menu.classList.remove("active");
+    });
+});
