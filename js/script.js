e048377791c608b194c69ecab27fa668abc92ed5
kusik16 <slobodyanuck@gmail.com>
2022-07-30 21:08:07 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const hamburger = document.querySelector(".hamburger"),
     closeElem = document.querySelector(".menu__close");
     overlay = document.querySelector(".menu__overlay");
     links = document.querySelectorAll(".menu__link");
+    mybutton = document.querySelector(".pageup");
+    counters = document.querySelectorAll(".skills__ratings-counter"),
+    lines = document.querySelectorAll(".skills__ratings-line span");
 
 hamburger.addEventListener("click", () => {
     menu.classList.add("active");
@@ -16,8 +19,7 @@ overlay.addEventListener("click", () => {
     menu.classList.remove("active");
 });
 
-const counters = document.querySelectorAll(".skills__ratings-counter"),
-    lines = document.querySelectorAll(".skills__ratings-line span");
+mybutton.addEventListener("click", topFunction);
 
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
@@ -28,3 +30,28 @@ links.forEach((item) => {
         menu.classList.remove("active");
     });
 });
+
+//Get the button:
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 1500 ||
+        document.documentElement.scrollTop > 1500
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.documentElement.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+}
